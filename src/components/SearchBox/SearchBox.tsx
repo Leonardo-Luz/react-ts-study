3f84df6d0c6bfa99ab079eef5bd396232b9c6a49
Leonardo-Luz <leonardoluz07@gmail.com>
2023-09-26 17:42:58 -0300

    [wip] add search box

---
+++ src/components/SearchBox/SearchBox.tsx
@@ -0,0 +1,12 @@
+import './SearchBox.css';
+
+function SearchBox()
+{
+    return (
+        <input className='search' type='search' placeholder='pokemon...'>
+            
+        </input>
+    )
+}
+
+export default SearchBox;
