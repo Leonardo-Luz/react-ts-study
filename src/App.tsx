4fae8c3e832f604526cfec963de886d1195e56c6
Leonardo-Luz <leonardoluz07@gmail.com>
2023-10-01 16:33:32 -0300

    [feat] add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,149 +1,28 @@
-import React from 'react';
 import './App.css'
 import './components/navbar/Navbar'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Navbar from './components/navbar/Navbar';
-import Pokedex from './components/pokedex/Pokedex';
+import PokedexAll from './components/PokedexAll/PokedexAll';
+import GuessMain from './components/GuessMain/GuessMain';
 
-import { PokedataProps } from './types/PokedexProps';
-import data from "./data/Pokedata.json";
-
-type TypographyProps =
-{
-  children: React.ReactNode,
-  size?: number
-};
-
-const Title = ( { children , size = 3 }: TypographyProps) =>
-{
-  return(
-    <h1
-      style={{
-        fontSize: size + "rem",
-      }}
-    >
-    
-      {children}
-    
-    </h1>
-  )
-};
-
-type AppState = {
-  Pokedata: PokedataProps[],
-  searchedPokemon: PokedataProps[],
-  selectedPokemon: PokedataProps | undefined
-}
-
-let missing = 
-[
-{  "id": 0,
-  "name": {
-    "english": "MissingNo",
-    "japanese": "MissingNo",
-    "chinese": "MissingNo",
-    "french": "MissingNo"
-  },
-  "type": [
-    "Not Found"
-  ],
-  "base": {
-    "HP": 0,
-    "Attack": 0,
-    "Defense": 0,
-    "Sp. Attack": 0,
-    "Sp. Defense": 0,
-    "Speed": 0
-  }
-}
-] as PokedataProps[]
-
-class App extends React.Component<any, AppState > {
-
-  state = {
-    Pokedata: data,
-    searchedPokemon: data,
-    selectedPokemon: undefined
-  };
-
-  HandleInputChange = (inputValue: string) =>
-  {
-    const  { Pokedata } = this.state;
-    
-    let searchedPokemon = Pokedata.filter(
-          ( pokemon ) => {
-              return (
-                pokemon.name.english  &&
-                pokemon.name.english.toLowerCase().includes(inputValue.toLowerCase()) ||
-                pokemon.type[0] &&
-                pokemon.type[0].toLowerCase().includes(inputValue.toLowerCase()) ||
-                pokemon.type[1] &&
-                pokemon.type[1].toLowerCase().includes(inputValue.toLowerCase()) ||                
-                pokemon.type[2] &&
-                pokemon.type[2].toLowerCase().includes(inputValue.toLowerCase()) ||
-                pokemon.id &&
-                pokemon.id.toString().includes(inputValue)
-              )
-          }
-      )
-
-      if(searchedPokemon.length == 0)
-        searchedPokemon = missing    
-      
-
-      this.setState({
-        Pokedata: data,
-        searchedPokemon: searchedPokemon
-      });
-  }
-
-  HandleClick = (pokemonName: string) =>
-  {
-    const  { Pokedata } = this.state;
-  
-    console.log(pokemonName);
-
-    const selectedPokemon = Pokedata.find((pokemon: PokedataProps) => 
-        pokemon.name.english === pokemonName
-    );
-
-    console.log(selectedPokemon?.name);
-
-    if(selectedPokemon == this.state.selectedPokemon)
-      this.setState({selectedPokemon: undefined})
-    else
-      this.setState({selectedPokemon})
-  }
-
-  render(){    
+const App = () => {    
     return (
-      <div className="App">
-  
-        <header>
-          <Navbar/>
-        </header>
-  
-        <div className='margin'/>
-  
-        <main>
-          
-          <br />
-          <Title size = { 2 }>
-            POKEDEX
-          </Title>
-          <br />
+      <Router>
+        <div className="App">
 
-          <Pokedex 
-            searchedPokemon={this.state.searchedPokemon} 
-            selectedPokemon={this.state.selectedPokemon} 
-            onPokeCardClick={this.HandleClick} 
-            HandleInputChange={this.HandleInputChange}
-          />
-        </main>
-  
-  
-      </div>
+            <Navbar/>
+          
+            <div className='margin'/>
+
+            <Routes>
+              <Route path='/' element={<PokedexAll />} />
+              <Route path='/components/GuessMain' element={<GuessMain />} />
+            </Routes>
+        </div>
+      </Router>
     )      
-  }
 }
 
 export default App
