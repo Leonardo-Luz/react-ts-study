42780d314a010ffcfa1d492fe0f8f4ba2dc554c1
Leonardo-Luz <leonardoluz07@gmail.com>
2023-10-01 01:13:57 -0300

    [feat] add selected pokemon

--- src/components/PokeCard/PokeCard.tsx
+++ src/components/PokeCard/PokeCard.tsx
@@ -1,22 +1,29 @@
 import './PokeCard.css'
 
+ 
 type PokeCardProps = {
     id: number,
     name: string,
-    src: string
+    src: string,
+    type: string,
+    onPokeCardClick: (pokemonName: string) => void,
 };
 
-const PokeCard = ( { id , name , src }: PokeCardProps) =>
+const PokeCard = ( { id , name , src , type, onPokeCardClick }: PokeCardProps) =>
 {
 
+    // onPokeCardClick("Bulbasaur");
+
     return (
-        <div className='card'>
+        <button className='card' onClick={() => onPokeCardClick(name)}>
             <img src={src} alt={name + " image"} />
             <label className='card-space'>
                 <h3>{name}</h3>
                 <h3>#{id}</h3>
             </label>
-        </div>
+            <br />
+            <h3>{type}</h3>
+        </button>
     )
 }
 
